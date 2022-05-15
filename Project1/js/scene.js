b8a1bc04d1b096a8725a73d759a170efd5984023
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2022-05-15 14:50:56 +0100

    Testing the scene with a parallelepiped

--- Project1/js/scene.js
+++ Project1/js/scene.js
@@ -6,15 +6,38 @@ var geometry, material, mesh;
 
 var clock;
 
-var update_x = 0, update_y = 0, update_z = 0;
+var update_x, update_y, update_z, update_v1, update_v2, update_v3;
+
+var parallelepiped;
+
+// This is the only parallelpiped, so no coordinates are needed
+function createParallelepiped(){
+    'use strict';
+    
+    parallelepiped = new THREE.Object3D();
+
+    material = new THREE.MeshBasicMaterial({ color: 0x00FFFF, wireframe: true });
+    geometry = new THREE.BoxGeometry(Math.sqrt(52), Math.sqrt(512), Math.sqrt(52));
+    mesh = new THREE.Mesh(geometry, material);
+
+    parallelepiped.add(mesh);
+    parallelepiped.position.set(11, 42, 0);
+    parallelepiped.rotation.set(0, 0, Math.PI / 4);
+
+    scene.add(parallelepiped);
+}
+    
 
 function createScene() {
     'use strict';
 
     scene = new THREE.Scene();
 
-    scene.add(new THREE.AxisHelper(10));
+    // scene.background = new THREE.Color( 0xD5B895 );
 
+    scene.add(new THREE.AxisHelper(50));
+    
+    createParallelepiped();
 }
 
 function createCamera(){
@@ -24,13 +47,14 @@ function createCamera(){
                                          1,
                                          1000);
     useFullViewCamera();
-    camera.lookAt(scene.position);
 }
 
 function useFullViewCamera() {
-    camera.position.x = 10;
-    camera.position.y = 10;
-    camera.position.z = 10;
+    camera.position.x = 80;
+    camera.position.y = 80;
+    camera.position.z = 80;
+    console.log(scene.position);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 
 function useTopViewCamera() {
@@ -38,7 +62,10 @@ function useTopViewCamera() {
 }
 
 function useSideViewCamera() {
-    // TODO: Define positions
+    camera.position.x = 0;
+    camera.position.y = 26;
+    camera.position.z = 80;
+    camera.lookAt(new THREE.Vector3(0, 26, 0));
 }
 
 function onResize() {
@@ -53,6 +80,7 @@ function onResize() {
 
 }
 
+// TODO: Allow multiple key press
 function onKeyDown(e) {
     'use strict';
 
@@ -62,9 +90,11 @@ function onKeyDown(e) {
         case 49:  // 1
             useFullViewCamera();
             break;
+
         case 50:  // 2
             useTopViewCamera();
             break;
+        
         case 51:  // 3
             useSideViewCamera();
             break;
@@ -83,37 +113,37 @@ function onKeyDown(e) {
         // Control v1 angle (main branch)
         case 81:  // Q
         case 113: // q
-            // TODO (Decrease v1 angle)
+            update_v1 = -1;
             break;
 
         case 87:  // W
         case 119: // w
+            update_v1 = 1;
             break;
-            // TODO (Increase v1 angle)
            
 
         // Control v2 angle (secondary branch)
         case 65:  // A
         case 97:  // a
-            // TODO (Decrease v2 angle)
+            update_v2 = -1;
             break;
 
         case 83:  // S
         case 115: // s
+            update_v2 = 1;
             break;
-            // TODO (Increase v2 angle)
         
 
         // Control v3 angle (tertiary branch)
         case 90:  // Z
         case 122: // z
+            update_v3 = -1;
             break;
-            // TODO (Decrease v3 angle)
         
         case 88:  // X
         case 120: // x
+            update_v3 = 1;
             break;
-            // TODO (Increase v3 angle)
 
             
         // Move along the axis (x, y and z)
@@ -145,6 +175,15 @@ function onKeyDown(e) {
     }
 }
 
+function reset_update_flags(){
+    update_x = 0;
+    update_y = 0;
+    update_z = 0;
+    update_v1 = 0;
+    update_v2 = 0;
+    update_v3 = 0;
+}
+
 function render() {
     'use strict';
     renderer.render(scene, camera);
@@ -162,6 +201,7 @@ function init() {
     createCamera();
     
     clock = new THREE.Clock();
+    reset_update_flags();
 
     render();
 
@@ -171,18 +211,20 @@ function init() {
 
 function animate() {
     'use strict';
-
-    var delta_r = 10 * clock.getDelta();
+    var delta_clock = clock.getDelta();
+    var delta_r = 10 * delta_clock;
+    var delta_v = Math.PI * delta_clock/4; 
 
     // Update camera position
     camera.position.x += update_x * delta_r;
     camera.position.y += update_y * delta_r;
     camera.position.z += update_z * delta_r;
 
-    // Update flags
-    update_x = 0;
-    update_y = 0;
-    update_z = 0;
+    // TODO: Update rotations based on delta_v
+
+    // Reset update flags
+    reset_update_flags();
+
     render();
 
     requestAnimationFrame(animate);
